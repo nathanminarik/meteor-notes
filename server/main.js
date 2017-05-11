1c44a2cb4516fed8a019bcc395b6a63213555eef
Nathan Minarik <nathanminarik@gmail.com>
2017-05-11 19:41:23 -0400

    First commit

---
+++ server/main.js
@@ -0,0 +1,11 @@
+import { Meteor } from 'meteor/meteor';
+import { WebApp } from 'meteor/webapp';
+import moment from 'moment';
+
+import './../imports/startup/simple-schema-configuration';
+import './../imports/api/users';
+
+
+Meteor.startup(() => {
+
+});
